57bb1d9248bf14d8fd7dff5bb2f53e26a70f9fd9
Cody Boggs <strofcon@gmail.com>
2017-09-12 22:19:15 -0600

    clean up hash functions and play with some other stuff (uncommitted)

--- main.go
+++ main.go
@@ -4,23 +4,48 @@ import (
 	"fmt"
 )
 
-func hashCountOddFromRight(x uint8, n uint8) uint8 {
-	y := uint8(0)
+func stringToInt(s string) uint32 {
+	x := uint32(0)
+	for _, r := range s {
+		x += uint32(r)
+	}
+	return x
+}
 
-	for i := range []uint8{0, 2, 4, 6} {
+func hashCountOddFromRight(x uint32, n uint32) uint32 {
+	y := uint32(0)
+
+	for i := 0; i < 16; i++ {
 		oddBit := x & 1
-		y |= (oddBit << uint8(i))
+		y |= (oddBit << uint32(i))
 		x >>= 2
 	}
 
 	return y % n
 }
 
-func main() {
-	x := uint8(247)
-	n := uint8(11)
+func hashCountEvenFromRight(x uint32, n uint32) uint32 {
+	y := uint32(0)
 
-	fmt.Printf("x>>0: %08b\n", x)
-	h1 := hashCountOddFromRight(x, n)
+	for i := 0; i < 16; i++ {
+		x >>= 1
+		evenBit := x & 1
+		y |= (evenBit << uint32(i))
+		x >>= 1
+	}
+
+	return y % n
+}
+
+func main() {
+	s := "hello world"
+	n := uint32(32)
+
+	sInt := stringToInt(s)
+	fmt.Printf("sInt: %d\n", sInt)
+	fmt.Printf("bin(sInt): %08b\n", sInt)
+	h1 := hashCountOddFromRight(sInt, n)
+	h2 := hashCountEvenFromRight(sInt, n)
 	fmt.Printf("h1: %d\n", h1)
+	fmt.Printf("h2: %d\n", h2)
 }
